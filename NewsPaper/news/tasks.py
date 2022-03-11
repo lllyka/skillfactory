5bbdb10b185c058858735d01d87a5057457dc15f
illyka <illyka@yandex.ru>
2022-03-11 14:57:27 +0300

    d9

--- NewsPaper/news/tasks.py
+++ NewsPaper/news/tasks.py
@@ -1,15 +1,15 @@
 from celery import shared_task
-import time
-from .views import MailSend,DetailView,subscribe,send_mail
+from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from .models import Post
 from django.core.mail import EmailMultiAlternatives
 
 @shared_task
-def post_mail_for_users():
+def post_mail_for_users(self, request, *args, **kwargs):
     post_mail = Post
+    example_dict = {str(Post.author): str(Post.category)}
     html_content = render_to_string(
-        'mail_created.html', )
+        'mail_created.html', example_dict)
     msg = EmailMultiAlternatives(
         subject=f'{post_mail.title} ',
         body=post_mail.text,  # это то же, что и message
@@ -19,4 +19,13 @@ def post_mail_for_users():
     msg.attach_alternative(html_content, "text/html")  # добавляем html
 
     msg.send()  # отсылаем
+
+@shared_task
+def mail_post(self, request, *args, **kwargs):
+    send_mail(
+        subject=f'{Post.title}',
+        message=f'Привет, новая статья в твоем разделе!',
+        from_email='ponialponyal@yandex.ru',
+        recipient_list=['illyka@yandex.ru']
+    )
 
