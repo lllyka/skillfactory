eb4a772c9c0d2af5ca1d0b2015318d006e920365
illyka <illyka@yandex.ru>
2022-03-16 17:45:26 +0300

    d9

--- NewsPaper/news/signals.py
+++ NewsPaper/news/signals.py
@@ -5,11 +5,11 @@ from django.conf import settings
 from django.template.loader import render_to_string
 from .models import Post
 
-"""@receiver(post_save, sender=Post)
+@receiver(post_save, sender=Post)
 def save_post(created, **kwargs):
     post_turn = kwargs['instance']
     subscribers_list = {user.email
-                        for category in post_turn.Category.all()
+                        for category in post_turn.category.all()
                         for user in category.subscribers.all()}
     email_from = settings.DEFAULT_FROM_EMAIL
 
@@ -19,8 +19,8 @@ def save_post(created, **kwargs):
     else:
         subject = 'В категориях, на которые вы подписаны, была изменена статья'
         text_message = f'В категориях, на которые вы подписаны,была изменена статья:'
-    html_template = render_to_string('post_create.html', {'post': post_turn, 'subject': subject})
+    html_template = render_to_string('send_mail.html', {'post': post_turn, 'subject': subject})
 
     msg = EmailMultiAlternatives(subject, text_message, email_from, list(subscribers_list))
     msg.attach_alternative(html_template, 'text/html')
-    msg.send()"""
+    msg.send()
