cd45c41c135635e3ad67eb49dcf2b01092c29712
illyka <illyka@yandex.ru>
2022-03-16 20:12:52 +0300

    d9

--- NewsPaper/news/signals.py
+++ NewsPaper/news/signals.py
@@ -1,9 +1,9 @@
 from django.dispatch import receiver
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save,  m2m_changed
 from django.core.mail import EmailMultiAlternatives
 from django.conf import settings
 from django.template.loader import render_to_string
-from .models import Post
+from .models import Post, Category
 
 @receiver(post_save, sender=Post)
 def save_post(created, **kwargs):
@@ -24,3 +24,24 @@ def save_post(created, **kwargs):
     msg = EmailMultiAlternatives(subject, text_message, email_from, list(subscribers_list))
     msg.attach_alternative(html_template, 'text/html')
     msg.send()
+
+
+@receiver(m2m_changed, sender=Post.category.through)
+def notify_managers_post(instance, action, pk_set, *args, **kwargs):
+    if action == 'post_add':
+        html_content = render_to_string(
+            'mail_created.html',
+            {'post': instance,
+             }
+        )
+        for pk in pk_set:
+            category = Category.objects.get(pk=pk)
+            recipients = [user.email for user in category.subscribers.all()]
+            msg = EmailMultiAlternatives(
+                subject=f'На сайте NewsPaper новая статья: {instance.title}',
+
+                from_email=settings.DEFAULT_FROM_EMAIL,
+                to=recipients
+            )
+            msg.attach_alternative(html_content, "text/html",)
+            msg.send()
--- testview/simpleapp/models.py
+++ testview/simpleapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator
-
+from django.shortcuts import reverse
+from django.core.cache import cache
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
@@ -8,6 +9,21 @@ class Product(models.Model):
     quantity = models.IntegerField(validators=[MinValueValidator(0, 'Quantity should be >= 0')])
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
     description = models.CharField(max_length=200)
+
+    # допишем свойство, которое будет отображать, есть ли товар на складе
+    @property
+    def on_stock(self):
+        return self.quantity > 0
+
+    def __str__(self):
+        return f'{self.name} {self.quantity}'
+
+    def get_absolute_url(self):  # добавим абсолютный путь, чтобы после создания нас перебрасывало на страницу с товаром
+        return f'/products/{self.id}'
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)  # сначала вызываем метод родителя, чтобы объект сохранился
+        cache.delete(f'product-{self.pk}')  # затем удаляем его из кэша, чтобы сбросить его
 
 
     def __str__(self):
