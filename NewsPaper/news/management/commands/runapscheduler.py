c27046fe27d0d00d5e9c5e62704bb8ade9b37826
illyka <illyka@yandex.ru>
2022-03-23 16:39:09 +0300

    d9

--- NewsPaper/news/management/commands/runapscheduler.py
+++ NewsPaper/news/management/commands/runapscheduler.py
@@ -1,5 +1,11 @@
 import logging
 
+from collections import defaultdict
+import datetime
+
+from django.template.loader import render_to_string
+from django.core.mail import EmailMultiAlternatives
+
 from django.conf import settings
 
 from apscheduler.schedulers.blocking import BlockingScheduler
@@ -8,9 +14,42 @@ from django.core.management.base import BaseCommand
 from django_apscheduler.jobstores import DjangoJobStore
 from django_apscheduler.models import DjangoJobExecution
 
-from django.core.mail import send_mail
+from news.models import Post
 
 logger = logging.getLogger(__name__)
+
+def send_posts(email_list, posts):
+    if isinstance(email_list, str):
+        subscriber_list = [email_list, ]
+    else:
+        subscriber_list = email_list
+
+    email_form = settings.DEFAULT_FROM_EMAIL
+    subject = 'В вашей любимой категории новая статья!'
+    text_message = 'В вашей любимой категории новая статья!'
+
+    render_html_template = render_to_string('send_posts.html', {'posts': posts, 'subject': subject})
+
+    msg = EmailMultiAlternatives(subject, text_message, email_form, list(subscriber_list))
+    msg.attach_alternative(render_html_template, 'text/html')
+    msg.send()
+
+
+def send_posts_weekly():
+
+    today = datetime.datetime.now()
+    last_week = today - datetime.timedelta(days=7)
+    last_week_posts_qs = Post.objects.filter(pubData__gte=last_week)
+
+    posts_for_user = defaultdict(set)
+
+    for post in last_week_posts_qs:
+        for category in post.category.all():
+            for user in category.subscribers.all():
+                posts_for_user[user].add(post)
+
+    for user, posts in posts_for_user.items():
+        send_posts(user.email, posts)
 
 
 
@@ -67,57 +106,3 @@ class Command(BaseCommand):
               )
         scheduler.my_job()
 
-'''logger = logging.getLogger(__name__)
-
-  
-# наша задача по выводу текста на экран
-def my_job():
-    #  Your job processing logic here...
-    print('hello from job')
-
-
-# функция, которая будет удалять неактуальные задачи
-def delete_old_job_executions(max_age=604_800):
-    """This job deletes all apscheduler job executions older than `max_age` from the database."""
-    DjangoJobExecution.objects.delete_old_job_executions(max_age)
-
-
-class Command(BaseCommand):
-    help = "Runs apscheduler."
-
-    def handle(self, *args, **options):
-        scheduler = BlockingScheduler(timezone=settings.TIME_ZONE)
-        scheduler.add_jobstore(DjangoJobStore(), "default")
-
-        # добавляем работу нашему задачнику
-        scheduler.add_job(
-            my_job,
-            trigger=CronTrigger(second="*/10"),
-            # То же, что и интервал, но задача тригера таким образом более понятна django
-            id="my_job",  # уникальный айди
-            max_instances=1,
-            replace_existing=True,
-        )
-        logger.info("Added job 'my_job'.")
-
-        scheduler.add_job(
-            delete_old_job_executions,
-            trigger=CronTrigger(
-                day_of_week="mon", hour="00", minute="00"
-            ),
-            # Каждую неделю будут удаляться старые задачи, которые либо не удалось выполнить, либо уже выполнять не надо.
-            id="delete_old_job_executions",
-            max_instances=1,
-            replace_existing=True,
-        )
-        logger.info(
-            "Added weekly job: 'delete_old_job_executions'."
-        )
-
-        try:
-            logger.info("Starting scheduler...")
-            scheduler.start()
-        except KeyboardInterrupt:
-            logger.info("Stopping scheduler...")
-            scheduler.shutdown()
-            logger.info("Scheduler shut down successfully!")'''
