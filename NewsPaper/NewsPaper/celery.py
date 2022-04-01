9c0a99e76c6b57a3f27c3701725fd02f62f5325f
illyka <illyka@yandex.ru>
2022-04-01 11:01:01 +0300

    d10

--- NewsPaper/NewsPaper/celery.py
+++ NewsPaper/NewsPaper/celery.py
@@ -4,14 +4,14 @@ from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NewsPaper.settings')
 
-app = Celery('news')
+app = Celery('NewsPaper')
 app.config_from_object('django.conf:settings', namespace='CELERY')
-
-app.autodiscover_tasks()
 
 app.conf.beat_schedule = {
     'action_every_monday_8am': {
-        'task': 'news.tasks.post_mail_for_users',
-        'schedule': crontab(),
+        'task': 'news.tasks.send_posts_to_email_weekly',
+        'schedule': crontab(hour=8, minute=0, day_of_week='monday'),
     },
 }
+
+app.autodiscover_tasks()
--- NewsPaper/news/tasks.py
+++ NewsPaper/news/tasks.py
@@ -1,31 +1,41 @@
-from celery import shared_task
 from django.core.mail import send_mail
+from celery import shared_task
+from django.conf import settings
+from news.models import Post
 from django.template.loader import render_to_string
-from .models import Post
+from datetime import datetime, timedelta
 from django.core.mail import EmailMultiAlternatives
+from django.utils import timezone
+from collections import defaultdict
 
 @shared_task
-def post_mail_for_users(self, request, *args, **kwargs):
-    post_mail = Post
-    example_dict = {str(Post.author): str(Post.category)}
-    html_content = render_to_string(
-        'mail_created.html', example_dict)
-    msg = EmailMultiAlternatives(
-        subject=f'{post_mail.title} ',
-        body=post_mail.text,  # это то же, что и message
-        from_email='ponialponyal@yandex.ru',
-        to=['illyka@yandex.ru'],  # это то же, что и recipients_list
-    )
-    msg.attach_alternative(html_content, "text/html")  # добавляем html
+def send_posts(email_list, posts):
+    if isinstance(email_list, str):
+        subscriber_list = [email_list, ]
+    else:
+        subscriber_list = email_list
 
-    msg.send()  # отсылаем
+    email_form = settings.DEFAULT_FROM_EMAIL
+    subject = 'В вашей любимой категории новая статья!'
+    text_message = 'В вашей любимой категории новая статья!'
 
-@shared_task
-def mail_post(self, request, *args, **kwargs):
-    send_mail(
-        subject=f'{Post.title}',
-        message=f'Привет, новая статья в твоем разделе!',
-        from_email='ponialponyal@yandex.ru',
-        recipient_list=['illyka@yandex.ru']
-    )
+    render_html_template = render_to_string('send_posts.html', {'posts': posts, 'subject': subject})
+
+    msg = EmailMultiAlternatives(subject, text_message, email_form, list(subscriber_list))
+    msg.attach_alternative(render_html_template, 'text/html')
+    msg.send()
+
+
+@shared_task()
+def send_posts_to_email_weekly():
+    week_posts = Post.objects.filter(time_post__gte=datetime.now(tz=timezone.utc) - timedelta(days=7))
+    posts_for_user = defaultdict(set)
+
+    for post in week_posts:
+        for category in post.categories.all():
+            for user in category.subscribers.all():
+                posts_for_user[user].add(post)
+
+    for user, posts in posts_for_user.items():
+        send_posts(user.email, posts)
 
